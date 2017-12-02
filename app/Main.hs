ce3ec11509c92d10354a80bc628d5c6eb129031b
Rien Maertens <maertensrien@gmail.com>
2017-12-03 00:27:22 +0100

    Day 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,25 @@
 module Main where
 
 import Days.Day1
+import Days.Day2
 import System.Environment
 
 main :: IO ()
 main = do
         args <- getArgs
         case args of
-            (a:as)   -> putStrLn $ day a as
+            (d:file:as) -> do
+                input <- readFile file
+                putStrLn "Input:"
+                putStrLn input
+                putStrLn "Result:"
+                putStrLn $ day d input
+
             _        -> putStrLn "Wrong number of args"
 
-day :: String -> [String] -> String
-day "1.1" = show . solveFirstCaptcha . head
-day "1.2" = show . solveSecondCaptcha . head
+day :: String -> String -> String
+day "1.1" = show . solveFirstCaptcha
+day "1.2" = show . solveSecondCaptcha
+day "2.1" = show . firstChecksum
+day "2.2" = show . secondChecksum
 
